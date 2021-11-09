8d6d0733e9eccf8c873572c803ab0b155a112e7e
0pin0 <pinchen0kimo@gmail.com>
2021-11-10 00:37:42 +0800

    [!+]改目錄

---
+++ web-mobile/src/settings.63a79.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"279e0",resources:"ed223",main:"e456f"}};
