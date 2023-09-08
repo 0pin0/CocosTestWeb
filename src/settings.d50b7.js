f3541902482b6a256698a9af441bf7bba986ebd4
0pin0 <pin_hot@outlook.com>
2023-09-08 23:41:05 +0800

    [+]test publish

---
+++ src/settings.d50b7.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Sample.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"2f4a7",main:"9cd26"}};
